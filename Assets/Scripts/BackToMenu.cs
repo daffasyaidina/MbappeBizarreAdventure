5e28a091477b548e4bb256afbd51ad2e9cf7ef8c
Theotrgl <91537576+Theotrgl@users.noreply.github.com>
2023-06-14 23:06:01 +0700

    update

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -10,4 +10,9 @@ public class BackToMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
+    public void GoBackToMenu2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
 }
--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -9,4 +9,9 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void StartGame2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 }
